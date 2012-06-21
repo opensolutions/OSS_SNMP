eea71d81653d4e3e5d14d5241474273d1684baa3
Barry O'Donovan <barry@opensolutions.ie>
2012-06-21 16:34:33 +0100

    Start of a BGP MIB

---
+++ examples/bgp.php
@@ -0,0 +1,98 @@
+#! /usr/bin/php
+<?php
+
+/*
+    Copyright (c) 2012, Open Source Solutions Limited, Dublin, Ireland
+    All rights reserved.
+
+    Contact: Barry O'Donovan - barry (at) opensolutions (dot) ie
+    http://www.opensolutions.ie/
+
+    This file is part of the OSS_SNMP package.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    * Neither the name of Open Source Solutions Limited nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// This is an example script for OSS_SNMP Asterisk MIBs
+//
+// It needs to be called with a hostname / IP address and a community string
+
+if( count( $argv ) != 3 )
+{
+    echo <<< HELPTEXT
+
+OSS_SNMP - A PHP SNMP library for people who hate SNMP MIBs and OIDs!
+Copyright (c) 2012, Open Source Solutions Limited, Dublin, Ireland
+All rights reserved.
+
+See: https://github.com/opensolutions/OSS_SNMP/
+
+This is an example script to show how to use OSS_SNMP. It requires two arguments:
+
+ - the IP address of hostname of a SNMP capable host (with Asterisk SNMP enabled)
+ - the SNMP v2 community string for that host
+
+For example:
+
+    {$argv[0]} 192.168.10.20 public
+
+
+HELPTEXT;
+
+    exit( 1 );
+}
+
+require_once( dirname( __FILE__ ) . '/../OSS_SNMP/SNMP.php' );
+
+$host = new \OSS_SNMP\SNMP( $argv[1], $argv[2] );
+
+echo "\n\n";
+
+echo "BGP version running on {$argv[1]}: " . $host->useBGP()->version() . "\n";
+
+echo "BGP - local ASN: " . $host->useBGP()->localASN() . "\n";
+
+echo "BGP - peer identifiers: \n\n";
+
+print_r( $host->useBGP()->peerIdentifiers() );
+
+echo "\n\n";
+
+echo "BGP - peer states: \n\n";
+
+print_r( $host->useBGP()->peerConnectionStates(1) );
+
+echo "\n\n";
+
+
+echo "BGP - peer states: \n\n";
+
+print_r( $host->useBGP()->peerAdminStates(1) );
+
+echo "\n\n";
+
+
+exit( 0 );
+
+
