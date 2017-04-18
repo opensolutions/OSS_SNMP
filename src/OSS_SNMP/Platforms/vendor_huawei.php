f2b4c6f6e5677afbe1c2152b2d8b24b600cacb70
Barry O'Donovan <barry@opensolutions.ie>
2017-04-18 15:32:30 +0100

    Add Huawei S6720 - closes #41

--- src/OSS_SNMP/Platforms/vendor_huawei.php
+++ src/OSS_SNMP/Platforms/vendor_huawei.php
@@ -28,7 +28,24 @@ if( substr( strtolower($sysDescr), 0, 6 ) == 'huawei' )
     }
 
     $this->setOs( null );
-    $this->setOsVersion( $this->getSNMPHost()->useHuawei_System()->softwareVersion() );
+    if( $this instanceof \OSS_SNMP\TestPlatform ) {
+        $this->setOsVersion('PHPUnit');
+    } else {
+        $this->setOsVersion($this->getSNMPHost()->useHuawei_System()->softwareVersion());
+    }
     $this->setOsDate( null );
 
 }
+
+// 'S6720-30C-EI-24S-AC Huawei Versatile Routing Platform Software VRP (R) software,Version 5.160 (S6720 V200R009C00SPC500) Copyright (C) 2007 Huawei Technologies Co., Ltd.'
+// https://github.com/opensolutions/OSS_SNMP/issues/41
+else if( preg_match( '/^S\d+\-[A-Z0-9\-]+ Huawei Versatile Routing Platform Software VRP \(R\) software,Version ([0-9\.]+) \((S[0-9]+) [A-Z0-9]+\) Copyright \(C\) 2007 Huawei Technologies.*$/',
+                $sysDescr, $matches ) ) {
+
+    $this->setVendor( 'Huawei' );
+    $this->setModel( $matches[2] );
+    $this->setOs( 'Huawei Versatile Routing Platform Software VRP' );
+    $this->setOsVersion( $matches[1] );
+    $this->setOsDate( null );
+}
+
---
+++ tests/OSS_SNMP/Platforms/HuaweiTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: barryo
+ * Date: 18/04/2017
+ * Time: 13:59
+ */
+
+namespace Tests\OSS_SNMP\Platforms;
+
+use OSS_SNMP\TestPlatform as TestOSSPlatform;
+
+class HuaweiTest extends Platform
+{
+
+    const HUAWEI_A = 'Huawei Integrated Access Software';
+
+    public function testHuaweiA() {
+
+        $p = new TestOSSPlatform( self::HUAWEI_A, '.1.3.6.1.4.1.2011.2.123' );
+
+        $this->assertEquals( $p->getVendor(),    'Huawei' );
+        $this->assertNull( $p->getOs() );
+        $this->assertEquals( $p->getOsVersion(), 'PHPUnit' );
+        $this->assertNull( $p->getOsDate() );
+        $this->assertEquals( $p->getModel(),     'MA5603T' );
+    }
+
+    const HUAWEI_B = 'HUAWEI TECH. INC. SNMP AGENT FOR MA5300';
+
+    public function testHuaweiB() {
+
+        $p = new TestOSSPlatform( self::HUAWEI_A, '.1.3.6.1.4.1.2011.2.6.6.1' );
+
+        $this->assertEquals( $p->getVendor(),    'Huawei' );
+        $this->assertNull( $p->getOs() );
+        $this->assertEquals( $p->getOsVersion(), 'PHPUnit' );
+        $this->assertNull( $p->getOsDate() );
+        $this->assertEquals( $p->getModel(),     'MA5300V1' );
+    }
+
+    const HUAWEI_C = 'S6720-30C-EI-24S-AC Huawei Versatile Routing Platform Software VRP (R) software,Version 5.160 (S6720 V200R009C00SPC500) Copyright (C) 2007 Huawei Technologies Co., Ltd.';
+
+    public function testHuaweiC() {
+
+        $p = new TestOSSPlatform( self::HUAWEI_C, '' );
+
+        $this->assertEquals( $p->getVendor(),    'Huawei' );
+        $this->assertEquals( $p->getOs(), 'Huawei Versatile Routing Platform Software VRP' );
+        $this->assertEquals( $p->getOsVersion(), '5.160' );
+        $this->assertNull( $p->getOsDate() );
+        $this->assertEquals( $p->getModel(),     'S6720' );
+    }
+
+
+}
